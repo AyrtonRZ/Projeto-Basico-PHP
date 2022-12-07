2627ff4670035e0e455cba2717e6c1ecd2aa608d
Ayrton M. Rodrigues <109232580+AyrtonRZ@users.noreply.github.com>
2022-12-07 00:00:12 -0300

    add variaveis

---
+++ var.php
@@ -0,0 +1,25 @@
+<?php
+   $nome_do_projeto = "O Belo do Lixo";
+   $subtitulo = "Uma Exposição Visual de um Futuro Possível";
+   $numero_pronac = 204502;
+   $texto_infomartivo = "O Belo do Lixo é a realização de uma exposição de artes visuais que
+   será composta por fotografias projetadas em galerias e prédios e
+   escolhidas por meio de um concurso fotográfico. Além disso, será
+   realizado um concurso de redação tendo como tema: O Belo do Lixo em
+   que as pessoas poderão, através do registro de imagens e escrita
+   literária, demonstar a importância de um movimento cultural
+   mundialmente conhecido como Free Your Stuff - FYS, uma nova cultura /
+   filosofia que estimula as pessoas a doarem e ressignificarem objetos
+   que seriam descartados, ressignificando a ideia de consumo.";
+   $cidade = "João Pessoa";
+   $estado = "Paraíba";
+   $pais = "Brasil";
+   $valor_cota_minima = "R$ 10.000,00";
+   $valor_cota_namming = "R$ 207.299,84";
+   $proponente = "Parahybólica Agência e Produção Cultural EIRELI ME";
+   $cnpj = "20.306.831/0001-01";
+   $producao_executiva = "Parahybólica Cultural";
+   $realizacao = "Ponto de Coleta";
+   $objetivo_geral = "Ressignificar a relação da sociedade com o consumo apoiando movimentos culturais como o Free Your Stuff, um convite às práticas de expressão, desapego e consumo consciente.";
+   $publico_potencial_estimado = "75.000 pessoas (online e presencial).";
+?>
