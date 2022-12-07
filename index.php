13ed38b8e7b9fa0b08b047616b9c8632a9292778
Ayrton M. Rodrigues <109232580+AyrtonRZ@users.noreply.github.com>
2022-12-06 23:59:01 -0300

    add index

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// chamando as variáveis
+include "./var.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+  <?php
+  /* chamando o title */
+  include "./title.php";
+  ?>
+  <link href="./style.css" rel="stylesheet" />
+</head>
+
+<?php
+# chamando o body
+include "./body.php";
+?>
+
+</html>
